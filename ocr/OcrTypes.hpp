082603e4be17491ca2f8c4edb3fc4dfd78bcc940
bbo <bbo>
2022-04-07 05:18:55 +0000

    add status string map

--- ocr/OcrTypes.hpp
+++ ocr/OcrTypes.hpp
@@ -16,7 +16,7 @@ namespace ocr
     };
 
     // map the document type to argument string input
-    std::map<std::string, document_type> type_map{
+    static const std::map <std::string, document_type> type_map {
         {"card", document_type::card},
         {"pdf_diana", document_type::pdf_diana},
         {"pdf_uni", document_type::pdf_uni},
@@ -26,9 +26,21 @@ namespace ocr
     enum class status
     {
         ocr_success,
+        ocr_fail,
         no_error,
         unknown_doc_type,
-        file_not_found
+        file_not_found,
+        non_valid_image,
+        unknown
+    };
+
+    static const std::map <status, std::string> status_map {
+        {status::ocr_success,"ocr_success"},
+        {status::ocr_fail,"ocr_fail"},
+        {status::no_error,"no_error"},
+        {status::unknown_doc_type,"unknown_doc_type"},
+        {status::file_not_found,"file_not_found"},
+        {status::unknown,"unknown"}
     };
 
 }
